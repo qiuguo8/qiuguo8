86b05250f2229c347554b18c8415166da2e037e2
lijianping <361528511@qq.com>
2017-12-11 21:44:11 +0800

    提交

--- web/modules/manage/advertisemanage/vues/advertisemanage.vue
+++ web/modules/manage/advertisemanage/vues/advertisemanage.vue
@@ -1,44 +1,43 @@
 <template>
     <div class="advertise-manage">
         <div class="row-new text-center">
-            <div class="form-control content-60-to-60 float-left">
-                <label class="el-col-7">注册时间</label>
-                <div class="el-col-17">
-                    <span class="el-col-11">
-                        <mu-date-picker v-model="registerDate" hintText="选择时间"/>
-                    </span>
-                    <span class="el-col-2 mid-word">至</span>
-                    <span class="el-col-11">
-                        <mu-date-picker v-model="registerDate" hintText="选择时间"/>
-                    </span>
-                </div>
-            </div> 
+            
             <div class="form-control content-40-to-40 float-left">
                 <label class="el-col-7">标题</label>
                 <div class="el-col-17">
-                    <el-input v-model="title" placeholder="请输入标题"></el-input>
+                    <el-input v-model="bannerName" placeholder="请输入标题"></el-input>
                 </div>
             </div> 
+            <div class="form-control content-40-to-40 float-left">
+                <label class="el-col-9 text-center">启用</label>
+                <el-select class="el-col-15" v-model="enabled" filterable placeholder="请选择">
+                    <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                    </el-option>
+                </el-select>
+            </div>
             <div class="form-control el-col-24 text-center">
-                <el-button type="primary">查询</el-button>
+                <el-button type="primary" @click="submitForm()">查询</el-button>
                 <el-button >重置</el-button>
                 <el-button type="danger" @click="add()">新增</el-button>
             </div>  
         </div>
         <div class="el-col-24">
-            <el-table :default-sort="{prop:'count',order:'ascending'}" :data="tableData3" border>
-                <el-table-column prop="index" label="标题" min-width="50" align="center" head-align="center" class-name="table-fixed"></el-table-column>
-                <el-table-column prop="userName" label="图片URL" min-width="80" align="center" head-align="center" class-name="table-fixed"> </el-table-column>
-                <el-table-column prop="account" label="正文链接" min-width="60" align="center" head-align="center" class-name="table-fixed"></el-table-column>
-                <el-table-column prop="mobileNo" label="是否启用" min-width="80" align="center" head-align="center" class-name="table-fixed"></el-table-column>
-                <el-table-column prop="mail" label="创建时间" min-width="80" align="center" head-align="center" class-name="table-fixed"></el-table-column>
-                <el-table-column prop="registerDate" label="修改时间" min-width="70" align="center" head-align="center" class-name="table-fixed"></el-table-column>
-                <el-table-column prop="status" label="创建人" min-width="60" align="center" head-align="center" class-name="table-fixed"></el-table-column>
+            <el-table :default-sort="{prop:'count',order:'ascending'}" :data="datatable" border>
+                <el-table-column prop="bannerName" label="标题" min-width="50" align="center" head-align="center" class-name="table-fixed"></el-table-column>
+                <el-table-column prop="bannerUrl" label="图片URL" min-width="80" align="center" head-align="center" class-name="table-fixed"> </el-table-column>
+                <el-table-column prop="bannerLinkUrl" label="正文链接" min-width="60" align="center" head-align="center" class-name="table-fixed"></el-table-column>
+                <el-table-column prop="enabled" label="是否启用" min-width="80" align="center" head-align="center" class-name="table-fixed"></el-table-column>
+                <el-table-column prop="createTime" label="创建时间" min-width="80" align="center" head-align="center" class-name="table-fixed"></el-table-column>
+                <el-table-column prop="userName" label="创建人" min-width="60" align="center" head-align="center" class-name="table-fixed"></el-table-column>
                 <el-table-column label="操作" min-width="90" align="center" head-align="center" class-name="table-fixed" fixed="right">
                      <template slot-scope="scope">
-                        <el-button type="text" size="small">启用</el-button>
-                        <el-button type="text" size="small">禁用</el-button>
-                        <el-button type="text" size="small">修改</el-button>
+                        <el-button type="text" size="small" @click="updateBanner(scope.row,'1')">启用</el-button>
+                        <el-button type="text" size="small" @click="updateBanner(scope.row,'0')">禁用</el-button>
+                        <el-button type="text" size="small" @click="toUpdate(scope.row)">修改</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -46,11 +45,11 @@
                 <el-pagination
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
-                :current-page="currentPage4"
+                :current-page="currentPage"
                 :page-sizes="[10, 15, 20, 25]"
-                :page-size="15"
+                :page-size="pagesize"
                 layout=" prev, pager, next"
-                :total="400">
+                :total="totalCount">
                 </el-pagination>
             </div>
         </div>
@@ -59,6 +58,7 @@
 <script>
 import Vue from 'vue'
 import {Input,Button,Select,Option,Table,TableColumn,Pagination} from 'element-ui'
+import advertisemanageService from 'web/modules/manage/advertisemanage/service/advertisemanageService'
 Vue.component(Input.name,Input);
 Vue.component(Button.name,Button);
 Vue.component(Select.name,Select);
@@ -70,13 +70,19 @@ Vue.component(Pagination.name,Pagination);
 export default {
     data(){
         return {
-            tableData3:[
-                {index:'1',userName:'xx',account:'ddd',mobileNo:'1123456',mail:'xx@dd.com',registerDate:'2017-10-25',status:'xxx'},
-                {index:'1',userName:'xx',account:'ddd',mobileNo:'1123456',mail:'xx@dd.com',registerDate:'2017-10-25',status:'xxx'},
-                {index:'1',userName:'xx',account:'ddd',mobileNo:'1123456',mail:'xx@dd.com',registerDate:'2017-10-25',status:'xxx'},
-                {index:'1',userName:'xx',account:'ddd',mobileNo:'1123456',mail:'xx@dd.com',registerDate:'2017-10-25',status:'xxx'},
-                {index:'1',userName:'xx',account:'ddd',mobileNo:'1123456',mail:'xx@dd.com',registerDate:'2017-10-25',status:'xxx'}
-            ]
+            datatable:[],
+            options:[
+                {value: '', label: '全部' }, 
+                {value: '1', label: '启用' }, 
+                {value: '0',label: '禁用'}],
+            bannerName:null,
+            enabled:null, 
+            //当前页码
+            currentPage: 1,
+            //默认每页数据量
+            pagesize: 10,
+            //默认数据总数
+            totalCount: 0,
         }
     },
     mounted(){
@@ -85,7 +91,36 @@ export default {
     methods:{
         add(){
             this.$router.push({name:'advertise-edit'});
-        }
-    }
+        },
+        submitForm(){
+            this.query();
+        },
+        query(){
+            var sform= {'bannerName':this.bannerName,'enabled':this.enabled,'pageNum':this.currentPage,'pageSize':this.pagesize}
+            advertisemanageService.queryAllBanner(sform).then((ret)=>{
+                        this.datatable = ret.body.list;
+                        this.totalCount = ret.body.total;
+                    })
+        },
+        handleSizeChange(val){
+            this.pagesize = val;
+            this.query();
+        },
+        //页码变更
+        handleCurrentChange: function(val) {
+            this.currentPage = val;
+            this.query();
+        }, 
+        updateBanner(obj,status){
+            obj.enabled=status;
+            advertisemanageService.updateBanner(obj).then((ret)=>{
+                        alert( ret.body.status);
+                    })
+        },             
+
+    },
+    created:function () {
+        this.query();
+    },
 }
 </script>
