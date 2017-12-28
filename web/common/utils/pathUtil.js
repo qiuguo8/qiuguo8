b6cb872bbaa38a8394785ed0bee8749ef5b041e6
lijianping <361528511@qq.com>
2017-12-28 15:34:02 +0800

    更改静态文件地址

--- web/common/utils/pathUtil.js
+++ web/common/utils/pathUtil.js
@@ -4,7 +4,7 @@ const DEVELOPMENT = 'development';
 var curEnv = DEVELOPMENT;
 var origin = location.origin;
 var basePath = origin;
-var sataticPath = "http://119.23.69.45/";
+var sataticPath = "http://120.79.65.250/";
 
 if(curEnv === DEVELOPMENT){
     basePath += '/qiuguo8'; 
